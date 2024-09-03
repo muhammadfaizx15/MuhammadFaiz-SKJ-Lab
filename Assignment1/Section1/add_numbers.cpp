8555be6ca3c3f74561e34810644d6a50cbaa9394
muhammadfaizx15 <muhammadfaiz2005@mail.ugm.ac.id>
2024-09-03 16:57:46 +0700

    Add files via upload

---
+++ Assignment1/Section1/add_numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int x,y,sum;
+  x = 5;
+  y = 4;
+
+  sum = x + y;
+
+  // prints sum 
+  cout <<  " Sum:  " <<  sum;     
+
+  return 0;
+}
---
+++ Assignment1/Section2/x_number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int num1 = 5;
+    int num2 = 10;
+    int result = 0;
+
+    result = num1 * num2;
+
+    std::cout << "product: " << result << std::endl;
+
+    return 0;
+}
